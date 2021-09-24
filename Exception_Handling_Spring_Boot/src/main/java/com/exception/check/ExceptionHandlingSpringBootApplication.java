93e001b8850d7550df4e45d33cd22fa2a399ccea
shyampadma007 <shyamsanthosh22@gmail.com>
2021-09-24 22:22:17 +0530

    Add files via upload

---
+++ Exception_Handling_Spring_Boot/src/main/java/com/exception/check/ExceptionHandlingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.exception.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandlingSpringBootApplication.class, args);
+	}
+
+}
---
+++ Exception_Handling_Spring_Boot/src/test/java/com/exception/check/ExceptionHandlingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exception.check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
